a4adccc4edf0aec38a8238aa7572e2d75ccdc25f
mungaiDaniel <danitomonga@gmail.com>
2022-09-09 15:15:13 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alphabets;
+
+	for (alphabets = 0; alphabets < 10; alphabets++)
+	{
+		printf("%d", alphabets);
+	}
+	printf("\n");
+
+	return (0);
+}
+
