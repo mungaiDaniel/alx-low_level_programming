866197706e5fc185abfc9dcb264fb452fc636cf8
mungaiDaniel <danitomonga@gmail.com>
2022-09-13 16:27:46 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets in lowercases
+ * description: function void prints lower cases 
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet(void);
+
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+}
