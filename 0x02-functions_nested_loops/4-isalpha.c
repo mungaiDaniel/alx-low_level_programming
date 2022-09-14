0f5550c25af548280e75949c77304448400f07d1
mungaiDaniel <danitomonga@gmail.com>
2022-09-14 08:10:14 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - function to check lowercase character
+ * @c: is the int use for argument function
+ *
+ * Return: Alway 0 (success)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
