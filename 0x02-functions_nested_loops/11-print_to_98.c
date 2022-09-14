dbf10466c476e88e57585231126884264260802c
mungaiDaniel <danitomonga@gmail.com>
2022-09-14 11:14:22 +0000

    11-print_to_98.c correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,17 +12,18 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d, ", n--);
+			_putchar(n);
+			n--;
 		}
-		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
 		{
-			printf("%d, ", n++);
+			_putchar(n);
+			n++;
 		}
-		printf("%d\n", n);
+		_putchar('\n');
 	}
 }
 
