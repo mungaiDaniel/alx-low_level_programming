728c97fd891b129b591510ea474dc48d3afd43c1
mungaiDaniel <danitomonga@gmail.com>
2022-09-15 20:39:49 +0000

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a straight line
+ * @n: parameter
+ * Return: nothing(void)
+ */
+
+void print_diagonal(int n)
+{
+	int len;
+	int space;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+
+			if (len == (n - 1))
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
