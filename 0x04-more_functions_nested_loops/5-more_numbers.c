558ebd2de0cbc2838c7f7e2955770012ae373f8a
mungaiDaniel <danitomonga@gmail.com>
2022-09-15 17:31:15 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,7 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j > 9)
-			{
-				_putchar((j / 10) + '0');
-			}
-			_putchar((j % 10) + '0');
+			_putchar(j);
 		}
 	}
 	_putchar('\n');
