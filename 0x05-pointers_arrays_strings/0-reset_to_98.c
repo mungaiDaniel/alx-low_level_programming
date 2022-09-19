10803743e24f817cde079584ed3e96a38eb00968
mungaiDaniel <danitomonga@gmail.com>
2022-09-19 05:40:30 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - value to points to 98
+ * @n: parametares for the function
+ * Return: nothing(void)
+ */
+
+void reset_to_98(int *n)
+{
+	int p = 98;
+
+	n = &p;
+}
