fac4e57426a29dc9583bf910f319ba3137335be3
mungaiDaniel <danitomonga@gmail.com>
2022-09-19 12:05:14 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print a string in reverse
+ * @s: parametares for the function
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int lent;
+
+	lent = strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar('\n');
+}
