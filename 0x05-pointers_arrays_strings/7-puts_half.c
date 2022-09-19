0e1f16fd49bba27af2b0806ed68dcd79e5deb618
mungaiDaniel <danitomonga@gmail.com>
2022-09-19 13:04:18 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts_half - print even
+ * @str: parametares for the function
+ * Return: lent
+ */
+void puts_half(char *str)
+{
+	int x, i, y;
+
+	x = strlen(str);
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2;
+	for (i = 0; i < x; i++)
+		putchar(str[i]);
+	putchar('\n');
+
+
+}
