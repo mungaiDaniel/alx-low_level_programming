c3ed56ba3a914424c2b8c215aa5dc6c9bc112948
mungaiDaniel <danitomonga@gmail.com>
2022-09-19 12:38:11 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,19 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
-
 /**
- * puts2 - print even no
+ * puts2 - print even
  * @str: parametares for the function
- * Return: nothing
+ * Return: lent
  */
 void puts2(char *str)
 {
-	int lent;
+	int lent, i;
 
 	lent = strlen(str);
-
-	if (lent % 2 == 0)
-	{
-		putchar(lent);
-	}
+	for (i = 0; i < lent; i += 2)
+		putchar(str[i]);
 	putchar('\n');
+
+
 }
