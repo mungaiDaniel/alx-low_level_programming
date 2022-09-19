b25f5b2b4e9c96b1989323e2f8748542594d26fc
mungaiDaniel <danitomonga@gmail.com>
2022-09-19 13:19:11 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n element pf array
+ * @a: array
+ * @n: parametares for the function
+ * Return: lent
+ */
+void print_array(int *a, int n)
+{
+	int inc;
+
+	for (inc = 0; inc < n; inc++)
+	{
+		if (inc != n - 1)
+			printf("%d, ", a[inc]);
+		else
+			printf("%d", a[inc]);
+	}
+	putchar('\n');
+}
