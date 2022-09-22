77e096eababeedf8bccc73b609fe4b5e22d8ebe2
mungaiDaniel <danitomonga@gmail.com>
2022-09-21 23:15:43 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcmp - function that compare two strings
+ * @s1: first char argument
+ * @s2: second char argument
+ * Return: comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int result;
+
+	for (i = 0; i < n; i++)
+	{
+		result = strrev(s1[i]);
+	}
+
+
+	return (result);
+}
