0ce39fdf3e48ad6e8b33ee2fe3e4b0214af038f6
mungaiDaniel <danitomonga@gmail.com>
2022-09-22 04:01:18 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,26 +3,29 @@
 
 /**
  * leet - encoding a string
- * @str: argument passed
+ * @x: argument passed
  * Return: results
  */
-char *leet(char *str)
+char *leet(char *x)
 {
-	int indx1 = 0, indx2, l = 5;
-	char lee[5] = {'A', 'E', 'O', 'T', 'L'};
-	char laa[5] = {'4', '3', '0', '7', '1'};
+	int a = 0, b, l = 5;
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+	char trw[5] = {'4', '3', '0', '7', '1'};
 
-	while (str[indx1])
+	while (x[a])
 	{
-		for (indx2 = 0; indx2 < l; indx2++)
-		{
-			if (str[indx1] == lee[indx2] ||
-					(str[indx1] - 32) == laa[indx2])
-				str[indx1] = laa[indx2];
-		}
+		b = 0;
 
-		indx1++;
+		while (b < l)
+		{
+			if (x[a] == tr[b] || x[a] - 32 == tr[b])
+			{
+				x[a] = trw[b];
+			}
+			b++;
+		}
+		a++;
 	}
 
-	return (str);
+		return (x);
 }
