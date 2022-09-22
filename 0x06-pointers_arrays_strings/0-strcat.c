5f29f9a0131412f3a284fc9aff4dc23562b72323
mungaiDaniel <danitomonga@gmail.com>
2022-09-21 20:41:34 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	
+	int len = 0, i;
 
-	return (strcat(dest, src));
+	while (dest[len])
+	{
+		len++;
+	}
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
 
-	
+		len += 1;
+	}
+
+	dest[len] = '\0';
+
+	return (dest);
 }
