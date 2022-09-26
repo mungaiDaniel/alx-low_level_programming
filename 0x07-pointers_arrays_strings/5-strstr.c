1ecf175cfe4c31b64ab456728124db14a65d35da
mungaiDaniel <danitomonga@gmail.com>
2022-09-26 19:59:56 +0000

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @needle: string to check
+ * @haystack: character located
+ *
+ * Return: pointer to spot in s with c or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
+	while (*haystack)
+	{
+		index = 0;
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+				{
+					return (haystack);
+				}
+				index++;
+			} while (haystack[index] == needle[index]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
