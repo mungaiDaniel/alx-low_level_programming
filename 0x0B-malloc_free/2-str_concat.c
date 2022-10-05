90f0bd6c084b1ad01c8c9b28d10bdfe2788f200c
mungaiDaniel <danitomonga@gmail.com>
2022-10-04 20:15:15 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -52,11 +52,11 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		*(dst + i) = *(s1 + i);
-		for (j = 0; *(s2 + i) != '\0'; j++)
-		{
-			*(dst + i) = *(s2 + j);
-			i++;
-		}
+	for (j = 0; *(s2 + j) != '\0'; j++)
+	{
+		*(dst + i) = *(s2 + j);
+		i++;
+	}
 
 	return (dst);
 }
